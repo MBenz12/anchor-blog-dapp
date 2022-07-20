d11e742c99d1b6f549cadb0097581540d514f3f3
mbenz <mbenz10120112@gmail.com>
2022-07-20 12:06:34 -0400

    contract completed

--- programs/anchor-blog-dapp/src/lib.rs
+++ programs/anchor-blog-dapp/src/lib.rs
@@ -1,15 +1,104 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("56Wip2hmjkjVLAzqN8x7iiSc4hgUVL977pRFEaewo6CY");
+
+pub const BLOG_SEED_PREFIX: &str = "blog";
+pub const POST_SEED_PREFIX: &str = "post";
 
 #[program]
 pub mod anchor_blog_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_blog(
+        ctx: Context<InitializeBlog>, 
+        blog: Blog
+    ) -> Result<()> {
+        ctx.accounts.blog_account.set_inner(blog);
+        Ok(())
+    }
+
+    pub fn create_post(ctx: Context<CreatePost>, post: Post) -> Result<()> {
+        if (post.title.len() > 20) || (post.content.len() > 100) {
+            return Err(ErrorCode::InvalidContentOrTitle.into());
+        }
+        ctx.accounts.post_account.set_inner(post);
+        // ctx.accounts.blog_account.post_count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeBlog<'info> {
+    #[account(
+        init,
+        payer = authority,
+        seeds = [
+            BLOG_SEED_PREFIX.as_bytes(),
+            authority.key.as_ref(),
+        ],
+        bump,
+        space = Blog::LEN
+    )]
+    pub blog_account: Account<'info, Blog>,
+    
+    /// CHECK:    
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(post: Post)]
+pub struct CreatePost<'info> {
+    #[account(mut)]
+    pub blog_account: Account<'info, Blog>,
+
+    #[account(
+        init,
+        seeds = [
+            POST_SEED_PREFIX.as_bytes(),
+            blog_account.key().as_ref(),
+            post.slug.as_ref(),
+        ],
+        bump,
+        payer = authority,
+        space = Post::LEN,
+    )]
+    pub post_account: Account<'info, Post>,
+
+    /// CHECK:
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Blog {
+    pub authority: Pubkey,
+    pub bump: u8,
+    pub post_count: u8,
+}
+
+impl Blog {
+    const LEN: usize = 8 + 32 + 1 + 1;
+}
+
+#[account]
+pub struct Post {
+    pub author: Pubkey,
+    pub slug: String,
+    pub title: String,
+    pub content: String,
+}
+
+impl Post {
+    const LEN: usize = 8 + 32 + (4 + 10) + (4 + 20) + (4 + 100);
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Invalid Content or Title.")]
+    InvalidContentOrTitle
+}
--- tests/anchor-blog-dapp.ts
+++ tests/anchor-blog-dapp.ts
@@ -1,16 +1,88 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { AnchorBlogDapp } from "../target/types/anchor_blog_dapp";
+import { PublicKey, SystemProgram } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+type Blog = {
+  authority: PublicKey;
+  bump: number;
+  postCount: number;
+}
+
+type Post = {
+  author: PublicKey,
+  slug: String,
+  title: String,
+  content: String,
+}
 
 describe("anchor-blog-dapp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorBlogDapp as Program<AnchorBlogDapp>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const keypair = anchor.web3.Keypair.generate();
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL * 10), "confirmed"
+    );
+    const [blogAccount, blogBump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("blog"), 
+        keypair.publicKey.toBuffer()
+      ],
+      program.programId
+    );
+    const blog: Blog = {
+      authority: keypair.publicKey,
+      bump: blogBump,
+      postCount: 0,
+    };
+    await program.rpc.initializeBlog(blog, {
+      accounts: {
+        blogAccount: blogAccount,
+        authority: keypair.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [keypair]
+    });
+
+    const blogAccountData: Blog = await program.account.blog.fetchNullable(blogAccount);
+    console.log(blogAccountData);
+
+    const [postAccount, postBump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("post"),
+        blogAccount.toBuffer(),
+        Buffer.from("slug-1"),
+      ],
+      program.programId
+    );
+
+    const keypair1 = anchor.web3.Keypair.generate();
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(keypair1.publicKey, LAMPORTS_PER_SOL * 10), "confirmed"
+    );
+    const post: Post = {
+      author: keypair1.publicKey,
+      slug: "slug-1",
+      title: "first post",
+      content: "hello world!"
+    };
+    await program.rpc.createPost(post, {
+      accounts: {
+        postAccount,
+        authority: keypair1.publicKey,
+        blogAccount,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [keypair1]
+    });
+
+    const postData = await program.account.post.fetchNullable(postAccount);
+    console.log(postData);
   });
 });
